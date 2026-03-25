74ae3924f56d973f451c42cc3bae952449603cfc
hisu <callmehisu2211@gmail.com>
2026-03-26 01:21:26 +0700

    3

--- app/layout.js
+++ app/layout.js
@@ -1,39 +1,21 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const inter = Inter({
+  subsets: ["latin", "vietnamese"],
+  variable: "--font-inter",
 });
 
 export const metadata = {
   title: "Hisu Store - Web bán đồ decor và nội thất",
-  description: "Hisu Store - Chuyên đồ decor, nội thất nhỏ và phụ kiện trang trí hiện đại",
+  description:
+    "Hisu Store - Chuyên đồ decor, nội thất nhỏ và phụ kiện trang trí hiện đại",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="vi">
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="anonymous"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800&display=swap"
-          rel="stylesheet"
-        />
-      </head>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${inter.variable} antialiased`}>
         {children}
       </body>
     </html>
